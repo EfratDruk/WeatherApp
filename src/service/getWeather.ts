9224396472b8cd95a7d50a8e2f5ed9435da35610
Efrat <efratdr6179@gmail.com>
2025-06-08 13:43:47 +0300

    error handling

--- src/service/getWeather.ts
+++ src/service/getWeather.ts
@@ -2,18 +2,30 @@ const cities = ["Eilat", "New York", "London", "Alaska"];
 const API_KEY = "8ee633956bad6ae1965b557a94ecfcba";
 
 const getCordinats = async (city: string) => {
-  const res = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
-  );
-  const getData = await res.json();
-  if (!getData[0]) return null;
-  return { lat: getData[0].lat, lon: getData[0].lon };
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+    );
+    const getData = await res.json();
+    if (!getData[0]) return null;
+    return { lat: getData[0].lat, lon: getData[0].lon };
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+
+    return null;
+  }
 };
 
 const getWeather = async (lat: number, lon: number) => {
-  const weatherRes = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=he&appid=${API_KEY}`
-  );
+  let weatherRes;
+  try {
+    weatherRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=he&appid=${API_KEY}`
+    );
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    return null;
+  }
 
   const data = await weatherRes.json();
   return {
@@ -28,9 +40,9 @@ const getWeather = async (lat: number, lon: number) => {
 export const fetchWeatherData = async () => {
   return await Promise.all(
     cities.map(async (city) => {
-        const coords = await getCordinats(city);
-        if (!coords) return null;
-        return await getWeather(coords.lat, coords.lon);
+      const coords = await getCordinats(city);
+      if (!coords) return null;
+      return await getWeather(coords.lat, coords.lon);
     })
   );
 };
